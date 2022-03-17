cb1a8d336cf49fe428a207372e929f7a67f576a0
JosephFourier <fouriercrypto@protonmail.com>
2022-03-17 12:30:13 -0600

    gauges deploy

---
+++ vyper/contracts/Staking/C3_Variants/C3_StakingRewardsDualV5_UBO_KLIMA.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity >=0.6.11;
+pragma experimental ABIEncoderV2;
+
+import "../C3/C3_StakingRewardsDualV5.sol";
+
+contract StakingRewardsDualV5_UBO_KLIMA is C3StakingRewardsDualV5 {
+    constructor (
+        address _owner,
+        address _rewardsToken0,
+        address _rewardsToken1, // KLIMA is E9
+        address _stakingToken, 
+        address _c3_address,
+        address _timelock_address,
+        address _veC3_address
+    ) 
+    C3StakingRewardsDualV5(_owner, _rewardsToken0, _rewardsToken1, _stakingToken, _c3_address, _timelock_address, _veC3_address )
+    {}
+}
---
+++ vyper/contracts/Staking/C3_Variants/C3_UnifiedFarm_ERC20_KLIMA_NBO_KLIMA.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity >=0.8.0;
+pragma experimental ABIEncoderV2;
+
+import "../C3/C3_UnifiedFarm_ERC20.sol";
+
+contract C3UnifiedFarm_ERC20_KLIMA_NBO_KLIMA is C3UnifiedFarm_ERC20 {
+    constructor (
+        address _owner,
+        address[] memory _rewardTokens,
+        address[] memory _rewardManagers,
+        uint256[] memory _rewardRates,
+        address[] memory _gaugeControllers,
+        address[] memory _rewardDistributors,
+        address _stakingToken 
+    ) 
+    C3UnifiedFarm_ERC20(_owner , _rewardTokens, _rewardManagers, _rewardRates, _gaugeControllers, _rewardDistributors, _stakingToken)
+    {}
+}
